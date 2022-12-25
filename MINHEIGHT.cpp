08bb966697338119a65133e5010567c909d883b1
ken01234 <119032685+ken01234@users.noreply.github.com>
2022-12-25 22:08:31 +0800

    Add files via upload

---
+++ MINHEIGHT.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >>T;
+	while(T--){
+	    int X,H;
+	cin>> X>>H;
+	if(X<H){
+	    cout<<"No"<<endl;
+	}else if(X==H){
+	    cout<<"Yes"<<endl;
+	}else{
+	    cout<<"Yes"<<endl;
+	}
+	}
+	return 0;
+}
